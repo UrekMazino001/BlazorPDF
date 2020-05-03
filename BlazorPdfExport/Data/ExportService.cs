fa896f35a631bc902d74c93f08c1770259029aad
UrekMazino001 <46718869+UrekMazino001@users.noreply.github.com>
2020-05-03 13:15:36 -0600

    Add project files.

---
+++ BlazorPdfExport/Data/ExportService.cs
@@ -0,0 +1,71 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPdfExport.Data
+{
+    //export weather data to PDf document
+    public class ExportService
+    {
+        public MemoryStream CreatePdf(WeatherForecast[] weatherForecasts)
+        {
+            if(weatherForecasts == null)
+            {
+                throw new ArgumentException("Data cannot be null");
+            }
+            //Create a new PDF document
+
+            using (PdfDocument pdfDocument = new PdfDocument())
+            {
+                int paragraphAfterSpacing = 8;
+                int cellMargin = 8;
+
+                //Add page to the pdf Document
+                PdfPage page = pdfDocument.Pages.Add();
+
+                //create New Font
+                PdfStandardFont font = new PdfStandardFont(PdfFontFamily.TimesRoman, 16);
+
+                //Create a text elemet to draw a text in PDf Page
+                PdfTextElement title = new PdfTextElement("Weather Forecast", font, PdfBrushes.Black);
+                PdfLayoutResult result = title.Draw(page, new PointF(0, 0));
+
+
+                PdfStandardFont contentFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 12);
+                PdfLayoutFormat format = new PdfLayoutFormat();
+                format.Layout = PdfLayoutType.Paginate;
+
+
+                //Create PDF
+                PdfGrid pdfGrid = new PdfGrid();
+                pdfGrid.Style.CellPadding.Left = cellMargin;
+                pdfGrid.Style.CellPadding.Right = cellMargin;
+
+                //Apply built in Styke to the PDF grid
+                pdfGrid.ApplyBuiltinStyle(PdfGridBuiltinStyle.GridTable4Accent1);
+
+                //Assing Data Source
+                pdfGrid.DataSource = weatherForecasts.ToList();
+               
+                pdfGrid.Style.Font = contentFont;
+
+
+                //Draw PDf Grid into the pdf Page
+                pdfGrid.Draw(page,new PointF(0, result.Bounds.Bottom + paragraphAfterSpacing));
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    pdfDocument.Save(stream);
+                    pdfDocument.Close(true);
+                    return stream;
+                }
+            }
+        }
+    }
+}
